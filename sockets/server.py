23851d104395e8726b65a6f4bf15145d0bf93300
CarlosMabrey <75705516+CarlosMabrey@users.noreply.github.com>
2024-04-17 18:39:52 -0600

    Add files via upload

--- sockets/server.py
+++ sockets/server.py
@@ -3,7 +3,7 @@ from time import sleep
 
 # first of all import the socket library 
 
-server_ip = '10.0.6.126'
+server_ip = '10.0.5.70'
 server_port = 9999
     
 
@@ -21,27 +21,11 @@ print ("Server Socket successfully created")
 # coming from other computers on the network 
 s.bind((server_ip, server_port))         
 print ("socket binded to %s" %(server_port)) 
+print ("socket binded to %s" %(server_ip))
  
 # put the socket into listening mode 
 s.listen(5)     
 print ("socket is listening")            
- 
-# a forever loop until we interrupt it or 
-# an error occurs 
-#while True: 
- 
-  # Establish connection with client. 
-#  c, addr = s.accept()     
-#  print ('Got connection from', addr )
- 
-  # send a thank you message to the client. encoding to send byte type. 
-#  c.send('Thank you for connecting'.encode()) 
-
-  # receive data from the client
-#  data = c.recv(1024).decode()
-#  print(data)
-
-  # loop to continuously receive input from the client
 
 while True:
   c, addr = s.accept()  
